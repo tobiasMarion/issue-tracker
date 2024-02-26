0164909b72555f3385622abaab5ba117dbc1463e
Tobias Cadoná Marion <71554245+tobiasMarion@users.noreply.github.com>
2024-02-26 19:53:41 -0300

    Customizing Radix.UI themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,28 @@
-import './globals.css'
 import '@radix-ui/themes/styles.css'
+import './theme-config.css'
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Theme } from '@radix-ui/themes'
-
 import { NavBar } from './NavBar'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: 'Issue Tracker',
   description: 'First app with Next',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Theme appearance="light" accentColor="violet">
           <NavBar />
-
           <main className='p-5'>{children}</main>
         </Theme>
       </body>
