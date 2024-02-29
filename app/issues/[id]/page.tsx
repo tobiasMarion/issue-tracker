841ea126e2917bd87fe15b5826dbce8eb57cb217
Tobias Cadoná Marion <71554245+tobiasMarion@users.noreply.github.com>
2024-02-28 22:38:27 -0300

    Styling Issue Details Page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import prisma from "@/prisma/client"
 import { notFound } from 'next/navigation'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { IssueStatusBadge } from '@/app/components/IssueStatusBadge'
 
 interface Props {
   params: { id: string }
 }
 
 export default async function IssueDetailPage({ params }: Props) {
-  if (typeof params.id !== 'number') notFound()
-
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
   })
@@ -19,10 +19,14 @@ export default async function IssueDetailPage({ params }: Props) {
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="4" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>
+        {issue.createdAt.toDateString()}
+      </Card>
     </div>
   )
 }
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,7 +1,7 @@
-import { Table, TableRow } from "@radix-ui/themes";
+import { Table, TableRow } from "@radix-ui/themes"
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
-import { IssueActions } from "./IssueActions";
+import { IssueActions } from "./IssueActions"
 
 
 export default async function IssuesPage() {
