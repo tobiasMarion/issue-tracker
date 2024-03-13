e1929a318d8b5e89995ef79847aaf5c1d43144c5
Tobias Cadoná Marion <71554245+tobiasMarion@users.noreply.github.com>
2024-03-13 00:35:11 -0300

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/validationSchemas";
+import { patchIssueSchema } from "@/app/validationSchemas";
 import prisma from '@/prisma/client';
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -13,10 +13,19 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
   }
 
   const body = await request.json()
-  const validation = issueSchema.safeParse(body)
+  const validation = patchIssueSchema.safeParse(body)
 
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 })
+  }
+
+  const { assignedToUserId, title, description } = body
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({ where: { id: assignedToUserId } })
+
+    if (!user) {
+      return NextResponse.json({ error: 'Invalid User' }, { status: 400 })
+    }
   }
 
   const issue = await prisma.issue.findUnique({
@@ -29,10 +38,7 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
-    data: {
-      title: body.title,
-      description: body.description
-    }
+    data: { title, description, assignedToUserId }
   })
 
   return NextResponse.json(updatedIssue)
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,6 +1,25 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 export const issueSchema = z.object({
   title: z.string().min(1, 'Title is required').max(255),
-  description: z.string().min(1, 'Title is required')
-});
+  description: z.string().min(1, 'Title is required').max(65535, 'The new description is too long')
+})
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, 'Title is required')
+    .max(255)
+    .optional(),
+  description: z
+    .string()
+    .min(1, 'Title is required')
+    .max(65535, 'The new description is too long')
+    .optional(),
+  assignToUserId: z
+    .string()
+    .min(1, "AssignToUserId is required")
+    .max(255)
+    .optional()
+    .nullable()
+})
