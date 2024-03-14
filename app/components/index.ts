e7e9fc5963fbeb94c3d05617f3429f7f77e0853b
Tobias Cadoná Marion <71554245+tobiasMarion@users.noreply.github.com>
2024-03-14 11:48:15 -0300

    Building pagination component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,9 @@
 export { ErrorMessage } from './ErrorMessage'
+export { IssueDetails } from './IssueDetails'
 export { IssueStatusBadge } from './IssueStatusBadge'
 export { Link } from './Link'
 export { NavBar } from './NavBar'
+export { Pagination } from './Pagination'
 export { Skeleton } from './Skeleton'
 export { Spinner } from './Spinner'
-export { IssueDetails } from './IssueDetails'
+
